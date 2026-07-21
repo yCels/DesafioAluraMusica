3fb9d7f7f0a08acff9dcf6ebc1fb0d7bcc03a78c
yCels <celso5mf@gmail.com>
2026-07-21 16:14:59 -0300

    feat(modelo): cria classe base Audio com atributos e metodos iniciais

---
+++ src/minhasmusicas/modelo/_Audio.java
@@ -0,0 +1,37 @@
+package minhasmusicas.modelo;
+
+public class _Audio {
+    private String titulo;
+    private int totalRepoducoes;
+    private int totalCurtidas;
+    private double classificacao;
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalRepoducoes() {
+        return totalRepoducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public void curtir(){
+        this.totalCurtidas++;
+    }
+
+    public void reproduz(){
+        this.totalRepoducoes++;
+    }
+}
