1f6d37ceac5935e6d81dbfb1f57d9b70626e4de7
yCels <celso5mf@gmail.com>
2026-07-21 19:04:20 -0300

    feat(modelo) : cria modelo preferida para Audios com metodo a partir da nota de classificacao do audio

--- src/minhasmusicas/modelo/Preferidas.java
+++ src/minhasmusicas/modelo/Preferidas.java
@@ -1,4 +1,12 @@
 package minhasmusicas.modelo;
 
 public class Preferidas {
+
+    public void Inclui(Audio audio){
+        if(audio.getClassificacao() > 9){
+            System.out.println(audio.getTitulo()+ "é sucesso");
+        }else{
+            System.out.println(audio.getTitulo() + "é um dos mais escutados");
+        }
+    }
 }
